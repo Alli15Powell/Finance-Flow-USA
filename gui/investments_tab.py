8b8e66f8ac3928211fcb3d1603b25db5d1d29677
Allison Powell <ahp2221@jagmail.southalabama.edu>
2025-11-04 16:58:47 -0600

    Add QCompleter import to investments_tab.py

--- gui/investments_tab.py
+++ gui/investments_tab.py
@@ -1,6 +1,6 @@
 from PyQt5.QtWidgets import (
     QWidget, QVBoxLayout, QLabel, QPushButton,
-    QFileDialog, QLineEdit, QHBoxLayout, QMessageBox, QFormLayout
+    QFileDialog, QLineEdit, QFormLayout, QMessageBox, QCompleter
 )
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg
 import matplotlib.pyplot as plt
